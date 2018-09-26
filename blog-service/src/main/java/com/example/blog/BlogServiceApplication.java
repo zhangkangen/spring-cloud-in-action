7bb21f560a6d9d5a8471e310403127a3aecc35ad
zhangxiaokang <zhangxiaokang@bainuo.com>
2018-09-26 17:56:00 +0800

    blog service

---
+++ blog-service/src/main/java/com/example/blog/BlogServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class BlogServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BlogServiceApplication.class, args);
+    }
+}
